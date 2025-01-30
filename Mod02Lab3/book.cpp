e183981b4c40587213dd47bc1bd6c303795c2baa
LabComputer <lloyd.ivester@mymail.champlain.edu>
2025-01-30 11:20:10 -0500

    coded lol

---
+++ Mod02Lab3/book.cpp
@@ -0,0 +1,31 @@
+#include "book.h"
+
+Book::Book()
+{
+
+}
+
+Book::Book(const string& name, Type type, int pages, float ounces)
+{
+	bName = name;
+	bType = type;
+	bPages = pages;
+	bOunces = ounces;
+}
+
+string Book::formatReportLine() //return a string with all the info for the book
+{
+	string reportLine = bName + "\t| Type: " + getTypeName() + "  Pages: " + bPages + "  Weight(lbs): " + getWeightLbs();
+}
+
+float Book::getWeightLbs() //calculate and return the weight of the book in lbs
+{
+	const float POUNDS_PER_OUNCE = 0.0625;
+	float weightLbs = bOunces * POUNDS_PER_OUNCE;
+	return weightLbs;
+}
+
+string Book::getTypeName() //return the string which correlates with the book type
+{
+	
+}
